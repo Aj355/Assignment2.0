609a583a5e91349546ff562a89decccc647ca4e9
Aj355 <dahmi355@hotmail.com>
2017-11-11 05:35:01 -0400

    version 1.0.3

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void init_display ()
     print_str("\e[3J");  // Clear to top of terminal
     print_str("\e[;H");  // Cursor goes to top left
     //print_str("> ");     // Screen indication
-
+    //
 }
 void main(void)
  {
