3dc4c54f24e62121b1b55087a0a91d1ec132a646
Aj355 <dahmi355@hotmail.com>
2017-11-11 05:33:28 -0400

    version 1.0.2
    init display added

--- main.c
+++ main.c
@@ -10,10 +10,18 @@
 #include "kernel_calls.h"
 #include "Pcommands.h"
 
+void init_display ()
+{
+    print_str("\e[36m"); // Enable the Cyan font colour
+    print_str("\e[3J");  // Clear to top of terminal
+    print_str("\e[;H");  // Cursor goes to top left
+    //print_str("> ");     // Screen indication
+
+}
 void main(void)
  {
     init_kernel();
-
+    init_display ();
     /* reg_proc(process name, id, priority) */
     reg_proc(idle, 0, 0);
     reg_proc(proc1, 1, 3);
