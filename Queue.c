02a0758b90ee0d599166c890947b0b7a8084fb8f
Aj355 <dahmi355@hotmail.com>
2017-11-13 04:23:38 -0400

    version 2.6
    more comments and slightly clearer code

--- Queue.c
+++ Queue.c
@@ -3,11 +3,11 @@
  * Author: Abdullah     Alhadlaq
  * Course: Real time systems
  * ECED 4402
- * Date assigned :   07  Sept  2017
- * Date created  :   21  Sept  2017
- * Editing       :   29  Sept - Disable interrupt upon entry and enable upon
+ * Date assigned :   26  Sept  2017
+ * Date created  :   24  Oct  2017
+ * Editing       :   15  Sept - Disable interrupt upon entry and enable upon
  *                                  leaving
- * Submission date : 02 Oct 2017
+ * Submission date : 15 Nov 2017
  * File name : Queue.c
  * Purpose: Implement a static circular queue in order to organize interrupts
  *              According to their type (UART or SYSTICK)
--- process_support.c
+++ process_support.c
@@ -5,7 +5,20 @@
  *      Author: AbdullahAlhadlaq
  */
 
-
+/* -------------------------------------------------------------------------- *
+ * Author: Abdulrahman  Aljedaibi
+ * Author: Abdullah     Alhadlaq
+ * Course: Real time systems
+ * ECED 4402
+ * Date assigned :   26  Sept  2017
+ * Date created  :   24  Oct  2017
+ * Editing       :   15  Sept - Disable interrupt upon entry and enable upon
+ *                                  leaving
+ * Submission date : 15 Nov 2017
+ * File name : Queue.c
+ * Purpose: Implement a static circular queue in order to organize interrupts
+ *              According to their type (UART or SYSTICK)
+ * ------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
 #include "process_support.h"
@@ -23,6 +36,7 @@ void assignR7(volatile unsigned long data)
 */
 __asm(" mov r7,r0");
 }
+
 
 unsigned long get_PSP(void)
 {
